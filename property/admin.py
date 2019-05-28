864e55e9bf2bd82f4189feee36837bb60dbf2090
a4a <mukasaakram55@gmail.com>
2019-05-28 10:19:20 +0300

    ft(Admin login): Implements admin login
    - Edits admin page with landville logo, name and colors
    
    [Delivers #165907591]

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Property, PropertyReview, PropertyInspection, PropertyEnquiry
+from .models import (Property, PropertyReview,
+                     PropertyInspection, PropertyEnquiry)
 
 admin.site.register(Property)
 admin.site.register(PropertyReview)
