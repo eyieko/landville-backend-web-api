516e937b8558d97b804324d326c3bb533eea2f0d
crycetruly <crycetruly@gmail.com>
2019-07-29 13:02:57 +0300

    bg(emailBackend): Change Email backend to use Gmail
    - change the email backend to use gmail instead of Sendgrid
    
    [Starts #167564269]
    
    Co-authored-by: Akram Mukasa <mukasaakram55@gmail.com>

--- landville/settings.py
+++ landville/settings.py
@@ -162,7 +162,6 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_PORT = os.environ.get('EMAIL_PORT', '')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', '')
-SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 # Celery settings
