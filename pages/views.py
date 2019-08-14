8ab4a1bf38f2b7325261a911343da24a7ddc4e9a
crycetruly <crycetruly@gmail.com>
2019-08-14 11:01:48 +0300

    Add View docstrings

--- pages/views.py
+++ pages/views.py
@@ -7,9 +7,11 @@ from rest_framework import status
 
 class TermsView(APIView):
     """Handles sending terms and conditions to users."""
+
     serializer_class = PagesSerializer
 
     def get(self, request):
+        """Query for terms of use and serve them over http."""
         terms = Term.objects.latest('last_updated_at')
         serializer = PagesSerializer(terms)
         return Response(serializer.data, status=status.HTTP_200_OK)
